7e4d8513cdcbe1aec2d9b1e1049eccc81b279015
Hakim2Sky <m.hakim@outlook.com>
2025-07-29 07:05:46 +0300

    built function that plays a round

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    return prompt("Enter your Choice: ");
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    } else {
+        console.log(`its A TIE`);
+    }
+}
+
+playRound(getComputerChoice(), getHumanChoice());
